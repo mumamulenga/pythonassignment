f16673532e378076604d4f7ceb4b2d89cb3a5924
mumamulenga <101412173+mumamulenga@users.noreply.github.com>
2024-06-12 12:39:47 +0200

    Python assignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+# Ask the user for their name, age, and location
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Print out a personalized message
+print("Hello {name}, you are {age} years old and live in {location}.")
